4892a59a14e4ed497c2d582414db65bdc50488a4
Amal M Raju <amalraju178@gmail.com>
2024-06-05 16:10:56 +0530

    Various Primitive datatypes in java

---
+++ dtypes.java
@@ -0,0 +1,23 @@
+//various primitive datatypes in java 
+public class dtypes 
+{
+    public static void main(String args[])
+    {
+        char a='A';
+        byte b=4;
+        short c=345;
+        int d=2342;
+        long e=345432;
+        //float f=3.13;
+        double g=3.132;
+        System.out.println("char:"+a);
+        System.out.println("byte:"+b);
+        System.out.println("short"+c);
+        System.out.println("int:"+d);
+        System.out.println("long:"+e);
+        //System.out.println("float:"+f);
+        System.out.println("double:"+g);
+
+    }
+    
+}
